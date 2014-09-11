8049d3296196ccbfc414e28b6bcc4b5fdbdabed0
USER <USER@192.168.10.114>
2014-09-11 13:37:05 +0200

    forgot to add data to matchrune and matchmastery

--- src/main/java/my/api/stelar7/usewith/lol/dto/match/MatchMastery.java
+++ src/main/java/my/api/stelar7/usewith/lol/dto/match/MatchMastery.java
@@ -1,6 +1,12 @@
 package my.api.stelar7.usewith.lol.dto.match;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class MatchMastery
 {
-
+    long masteryId;
+    long rank;
 }
--- src/main/java/my/api/stelar7/usewith/lol/dto/match/MatchRune.java
+++ src/main/java/my/api/stelar7/usewith/lol/dto/match/MatchRune.java
@@ -1,6 +1,12 @@
 package my.api.stelar7.usewith.lol.dto.match;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class MatchRune
 {
-
+    long rank;
+    long runeId;
 }
