9b8fa8f9406ce301558d887717d657b2d52a215b
stelar7 <s.larssen@hotmail.com>
2014-11-04 00:46:57 +0100

    Added summoner spell enum for ease of access champion enum comming soon(tm)

--- src/test/java/test/StaticTest.java
+++ src/test/java/test/StaticTest.java
@@ -11,12 +11,11 @@ import org.junit.Test;
 public class StaticTest
 {
     L4J lib = new L4J("STILL HIDDEN!");
-
     @Test
     public void test1()
     {
         L4J.setRegion(Server.EUW);
-        SummonerSpell champ = lib.getStaticData().getSummonerSpellData(7, null, null, Arrays.asList("all"));
+        SummonerSpell champ = lib.getStaticData().getSummonerSpellData(SummonerSpell.Spells.EXHAUST.getId(), null, null, Arrays.asList("all"));
         System.out.println(champ.getSanitizedTooltip(18));
     }
 }
